21c9256e3bfde6d7ab31a803859ddd17d5925383
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2025-08-19 18:53:56 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,42 @@
 import { useState } from "react";
 
---- a/pages/index.js
-+++ b/pages/index.js
-@@
- const games = [
-   {
--    name: "1v1.lol",
--    url: "https://1v1.lol/", // Game URL
--    icon: "https://img.utdstc.com/icon/983/22a/98322a3b2be892eed31589906ffd949b68bcccc9a21ba562987965b5ec6bc6de:200", // URL to an external image
-+    name: "1v1.lol",
-+    url: "https://1v1.lol/",
-+    icon: "https://img.utdstc.com/icon/983/22a/98322a3b2be892eed31589906ffd949b68bcccc9a21ba562987965b5ec6bc6de:200",
-   },
-   {
--    name: "Game 2",
--    url: "https://newgame2url.com",
--    icon: "https://example.com/images/game2-icon.png", // URL to an external image
-+    name: "2048",
-+    url: "/games/2048/index.html",
-+    icon: "/games/icons/2048.png",
-+  },
-+  {
-+    name: "Snake",
-+    url: "/games/snake/index.html",
-+    icon: "/games/icons/snake.png",
-+  },
-+  {
-+    name: "Tetris",
-+    url: "/games/tetris/index.html",
-+    icon: "/games/icons/tetris.png",
-+  },
-+  {
-+    name: "Flappy Bird",
-+    url: "/games/flappy/index.html",
-+    icon: "/games/icons/flappy.png",
-+  },
-+  {
-+    name: "Pac-Man",
-+    url: "/games/pacman/index.html",
-+    icon: "/games/icons/pacman.png",
-+  },
-+  {
-+    name: "Minesweeper",
-+    url: "/games/minesweeper/index.html",
-+    icon: "/games/icons/minesweeper.png",
-+  },
-+  {
-+    name: "Breakout",
-+    url: "/games/breakout/index.html",
-+    icon: "/games/icons/breakout.png",
-   },
-   // Add more games here
- ];
+const games = [
+  {
+    name: "2048",
+    url: "/games/2048/index.html",
+    icon: "https://img.icons8.com/color/96/000000/2048.png",
+  },
+  {
+    name: "Snake",
+    url: "/games/snake/index.html",
+    icon: "https://img.icons8.com/color/96/000000/snake.png",
+  },
+  {
+    name: "Tetris",
+    url: "/games/tetris/index.html",
+    icon: "https://img.icons8.com/color/96/000000/tetris.png",
+  },
+  {
+    name: "Flappy Bird",
+    url: "/games/flappy/index.html",
+    icon: "https://img.icons8.com/color/96/000000/bird.png",
+  },
+  {
+    name: "Pac-Man",
+    url: "/games/pacman/index.html",
+    icon: "https://img.icons8.com/color/96/000000/pacman.png",
+  },
+  {
+    name: "Minesweeper",
+    url: "/games/minesweeper/index.html",
+    icon: "https://img.icons8.com/color/96/000000/minesweeper.png",
+  },
+  {
+    name: "Breakout",
+    url: "/games/breakout/index.html",
+    icon: "https://img.icons8.com/color/96/000000/breakout.png",
+  },
+];
 
 
 export default function Home() {
