0410a386b8835b1376536dbcc5949fc9e9fa526c
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2025-08-19 17:40:21 -0500

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const { password } = req.body;
 
   // Hardcoded password check (replace with your actual logic)
-  if (password === "nathan") {
+  if (password === "diddyblud") {
     return res.status(200).json({ success: true });
   } else {
     return res.status(401).json({ success: false });
