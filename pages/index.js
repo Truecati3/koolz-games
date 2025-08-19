fe5060ce26751b2fd6aeeea223ba36ebd5ffd9ee
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2025-08-19 18:43:43 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,102 @@ const games = [
     icon: "https://img.utdstc.com/icon/983/22a/98322a3b2be892eed31589906ffd949b68bcccc9a21ba562987965b5ec6bc6de:200",
   },
   {
-    name: "Game 2",
-    url: "https://newgame2url.com",
-    icon: "https://example.com/images/game2-icon.png",
+    name: "Run 3",
+    url: "https://run3.io/",
+    icon: "https://play-lh.googleusercontent.com/3GrNf6x0Ce4-hhczQ4F1hNYRm8YH6P-7jYwBZc5n-MtvWptLtxaFkGSllWJcXrIl6zU=w240-h480-rw",
   },
-  // Add more games here
+  {
+    name: "Slope",
+    url: "https://slopegame.online/",
+    icon: "https://play-lh.googleusercontent.com/-P2XAg2Yk7PBr5y6s8pYyVlyPpZuJHX8D5X1uEdvEJr-2QW1zYt3x2OD9E4p6OkU3g=w240-h480-rw",
+  },
+  {
+    name: "Retro Bowl",
+    url: "https://retrobowl.me/",
+    icon: "https://play-lh.googleusercontent.com/uEjANmNOnxz2jS0LljY0hOJvHWFlqgVcpIRSRYj8Z2Uwe4F7dOGQhW_pjHEJS0xjSOY=w240-h480-rw",
+  },
+  {
+    name: "Minecraft Classic",
+    url: "https://classic.minecraft.net/",
+    icon: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/80/Grass_Block_JE6_BE3.png",
+  },
+  {
+    name: "Basketball Stars",
+    url: "https://basketballstars.co/",
+    icon: "https://play-lh.googleusercontent.com/yvSP2mYuk0O1GlUF8dXsgtyyEMdIoX15mP9JzK4hNScCgL8h1u7VixIboWgX0o9nxg=w240-h480-rw",
+  },
+  {
+    name: "Drift Hunters",
+    url: "https://drift-hunters.com/",
+    icon: "https://play-lh.googleusercontent.com/EMKkb13gM3z72EVe6vJdPddY8ovS24-vzOYhXmm-SCg96QceF-LK7lRQW-OhBjXwUys=w240-h480-rw",
+  },
+  {
+    name: "Friday Night Funkin'",
+    url: "https://www.kbhgames.com/game/friday-night-funkin",
+    icon: "https://static.wikia.nocookie.net/fridaynightfunkin/images/f/f0/Boyfriend.png",
+  },
+  {
+    name: "Flappy Bird",
+    url: "https://flappybird.io/",
+    icon: "https://upload.wikimedia.org/wikipedia/en/0/0a/Flappy_Bird_icon.png",
+  },
+  {
+    name: "Cookie Clicker",
+    url: "https://orteil.dashnet.org/cookieclicker/",
+    icon: "https://play-lh.googleusercontent.com/I8uFhG_BeTWcF6RWQXtFUmSbA1u0Ztxm3rStNU7uWZzHmhzvA3ovMjzOPmUnE2N5iT0=w240-h480-rw",
+  },
+  {
+    name: "Google Snake",
+    url: "https://snake.googlemaps.com/",
+    icon: "https://play-lh.googleusercontent.com/D0kffUuKIV0R7HhG6cvNxkVsm1k3wJZsQ3bpM9IsdUnRzRP8KkX08nRtoCcQkR7yGQ=w240-h480-rw",
+  },
+  {
+    name: "Pac-Man",
+    url: "https://www.google.com/logos/2010/pacman10-i.html",
+    icon: "https://upload.wikimedia.org/wikipedia/en/5/59/Pac-man.png",
+  },
+  {
+    name: "Tetris",
+    url: "https://tetris.com/play-tetris",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Tetris.JPG",
+  },
+  {
+    name: "Crossy Road",
+    url: "https://crossyroad.io/",
+    icon: "https://play-lh.googleusercontent.com/5l0rjRkdqj7bHL52UjU5biAAQXkfbzjqD-9IY9cWiSFG6wHbBB48rjOVwJsv9p2U1Q=w240-h480-rw",
+  },
+  {
+    name: "Moto X3M",
+    url: "https://www.coolmathgames.com/0-moto-x3m",
+    icon: "https://play-lh.googleusercontent.com/LzN12Ap3MPe1hA8upTuwfZeq5r0fWbbhF0mSUh5F1gpiE8JqI_4eVJ9imT24FcQjRrM=w240-h480-rw",
+  },
+  {
+    name: "Shell Shockers",
+    url: "https://shellshock.io/",
+    icon: "https://shellshock.io/img/favicon.png",
+  },
+  {
+    name: "Krunker",
+    url: "https://krunker.io/",
+    icon: "https://krunker.io/img/favicon.png",
+  },
+  {
+    name: "Paper.io 2",
+    url: "https://paper-io.com/",
+    icon: "https://play-lh.googleusercontent.com/LPJZtW9k5WjWW8R8QH1srX3mw7w2D-w6A3iKqMThZz5JJGg0jipbL2Y4mjlw4U9mZFo=w240-h480-rw",
+  },
+  {
+    name: "Agar.io",
+    url: "https://agar.io/",
+    icon: "https://play-lh.googleusercontent.com/WAvzkdxl3p2kCwmtDiPf1n3foBMp35zk1k8KeEuC1IuQW08BKnGJjbQzUuIDGFhftmA=w240-h480-rw",
+  },
+  {
+    name: "Slither.io",
+    url: "https://slither.io/",
+    icon: "https://slither.io/s/fav/snake.png",
+  },
 ];
+
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
