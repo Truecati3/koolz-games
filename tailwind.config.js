f8809163a2709717ef8175944c8e27136541141c
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2025-08-20 02:18:16 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",      // All files in /pages
+    "./components/**/*.{js,ts,jsx,tsx}", // All files in /components
+    "./app/**/*.{js,ts,jsx,tsx}",        // If you’re using Next.js App Router
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
