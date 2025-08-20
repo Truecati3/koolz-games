13a1c02de7a8c1c9d48a4a2a99167c017060700e
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2025-08-19 20:34:36 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
-// pages/_app.js
-import '../styles/globals.css'
+import '@/styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { auth } from '../firebase'
+import { onAuthStateChanged } from 'firebase/auth'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user && router.pathname !== '/login') {
+        router.push('/login')
+      }
+    })
+    return () => unsubscribe()
+  }, [router])
+
   return <Component {...pageProps} />
 }
