c62f721e73f54470eec5e013a031e59d643f93bb
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2025-08-19 19:34:22 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCL44DAf-deKKKJLl-At8pCmyAMNGjQvjI",
   authDomain: "koolz-games.firebaseapp.com",
   projectId: "koolz-games",
-  storageBucket: "koolz-games.firebasestorage.app",
+  storageBucket: "koolz-games.appspot.com",
   messagingSenderId: "621336210290",
   appId: "1:621336210290:web:4fcb1cbdf24dc896994beb",
   measurementId: "G-1KDW80FSZ6"
@@ -17,10 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Auth
-const auth = getAuth(app);
-
-// Analytics (disable on server)
-const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
-
-export { auth, analytics };
+// Export Auth + Analytics
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
