34564a5d3265bf6a9f09d5b9f5df8b30b06e7e25
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2025-08-20 00:42:27 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,4 @@
-
-
-export default app;
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
@@ -11,10 +9,12 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
+// Auth exports
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
