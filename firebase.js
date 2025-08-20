b8c01a0a85d1edd0d0bc569c0f3e08d3bbf8ee4c
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2025-08-19 23:03:08 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// If you later add Firestore: import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -15,6 +16,9 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+
+// Auth + Firestore
 export const auth = getAuth(app);
-// export const db = getFirestore(app) // if/when you add Firestore
+export const db = getFirestore(app);
+
 export default app;
