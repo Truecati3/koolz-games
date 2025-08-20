89eff43730170ec05feaeadcb57bbd6bdd5e8fd2
Truecati3 <99998245+Truecati3@users.noreply.github.com>
2025-08-20 16:10:39 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-// /pages/_app.js
+// pages/_app.js
 import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
   );
 }
+
+export default MyApp;
